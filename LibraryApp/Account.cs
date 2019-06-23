ef8370919d712f84f05cd317d361b2248bf6f760
Satya P Javvadi <saphin.rao@gmail.com>
2019-06-22 23:15:32 -0700

    Library App initial version

---
+++ LibraryApp/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp
+{
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+        #region Properties            
+
+        /// <summary>
+        /// The account number
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        /// <summary>
+        /// The email id for the account
+        /// </summary>
+        public string  EmailId { get; set; }
+        /// <summary>
+        /// The account created time
+        /// </summary>
+        public DateTime AccountCreated { get; private set; }
+        /// <summary>
+        /// The number of books checked out on this account
+        /// </summary>
+        public int CheckedoutBooksCount { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+            AccountCreated = DateTime.Now;
+            CheckedoutBooksCount = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Check out books into the account
+        /// </summary>
+        /// <param name="bookCount">Number of books to be checked out</param>
+        /// <returns>Total checked out book count on the account</returns>
+        public int CheckoutBooks(int bookCount)
+        {
+            CheckedoutBooksCount += bookCount;
+            return CheckedoutBooksCount;
+            
+        }
+
+        /// <summary>
+        /// Check in books from the account
+        /// </summary>
+        /// <param name="bookCount">The number of books to be checked in</param>
+        /// <returns>Total checked out book count on the account</returns>
+        public int CheckinBooks(int bookCount)
+        {
+            CheckedoutBooksCount -= bookCount;
+            return CheckedoutBooksCount;
+        }
+        
+        #endregion
+    }
+}
---
+++ LibraryApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the account
+            var account1 = new Account()
+            {
+                EmailId = "test1@gmail.com"
+            };
+           
+            Console.WriteLine($"Account Number: {account1.AccountNumber}, Email: {account1.EmailId}, Created Date: {account1.AccountCreated}, Checkedout Books: {account1.CheckedoutBooksCount}");
+
+            account1.CheckoutBooks(4);
+
+            Console.WriteLine($"Account Number: {account1.AccountNumber}, Email: {account1.EmailId}, Created Date: {account1.AccountCreated}, Checkedout Books: {account1.CheckedoutBooksCount}");
+
+            account1.CheckinBooks(2);
+
+            Console.WriteLine($"Account Number: {account1.AccountNumber}, Email: {account1.EmailId}, Created Date: {account1.AccountCreated}, Checkedout Books: {account1.CheckedoutBooksCount}");
+
+            // Create second instance of the account
+            var account2 = new Account()
+            {
+                EmailId = "test2@gmail.com"
+            };
+
+            Console.WriteLine($"Account Number: {account2.AccountNumber}, Email: {account2.EmailId}, Created Date: {account2.AccountCreated}, Checkedout Books: {account2.CheckedoutBooksCount}");
+        }
+    }
+}
